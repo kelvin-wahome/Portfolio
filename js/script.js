aeb8dd5ab09e18d59722756706d5d6ae8d8e87e2
blackrose <wahomekelving@gmail.com>
2019-04-17 17:52:06 +0300

    modified website

--- js/script.js
+++ js/script.js
@@ -1,84 +1,160 @@
-$(document).ready(function() {
-  window.onload = function () {
-    typeWriter()
-  };
-  var i = 0;
-  var txt = 'Hello, my name is Gift Lumumba';
-  var speed = 200;
+var app = angular.module('portfolio', [])
 
-  function typeWriter() {
-      if (i < txt.length) {
-          document.getElementById("demoo").innerHTML += txt.charAt(i);
-          i++;
-          setTimeout(typeWriter, speed);
+//navigation controller
+.controller('navCtrl', function($scope) {
+  //navigation menu items
+  $scope.navigation = [{
+    id: '#about',
+    name: 'About'
+  }, {
+    id: '#portfolio',
+    name:
+function main() {
+
+    (function () {
+        'use strict';
+      /* ==============================================
+         Page Scrolling
+         =============================================== */
+
+        $('a.scroll-nav').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 40
+                    }, 900);
+                    return false;
+                }
+            }
+        });
+
+        /*====================================
+         Show Menu on Book
+         ======================================*/
+        $(window).bind('scroll', function() {
+            var navHeight = $(window).height() - 100;
+            if ($(window).scrollTop() > navHeight) {
+                $('.navbar-default').addClass('on');
+            } else {
+                $('.navbar-default').removeClass('on');
+            }
+        });
+        /* ==============================================
+         Scroll Spy
+         =============================================== */
+        $('body').scrollspy({
+            target: '.navbar-default',
+            offset: 80
+        })
+
+       }());
       }
-  }
+main();'Portfolio'
+  }, {
+    id: '#contact',
+    name: 'Contacts'
+  }, ];
 
-  // navbar initially hidden
-  $('.navbar').hide();
+  // Highlight the top nav as scrolling occurs
+  $('body').scrollspy({
+    target: '.navbar-fixed-top'
+  })
 
-  // display tooltip on page load
-  $('.tooltip').tooltip('show');
-
-  var scrollLink = $('.sections');
-
-  // Smooth scrolling
-  scrollLink.click(function(e) {
-    e.preventDefault();
-    $('body,html').animate({
-      scrollTop: $(this.hash).offset().top
-    }, 1000 );
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-toggle:visible').click();
   });
 
-  // Active link switching
-  $(window).scroll(function() {
-    var scrollbarLocation = $(this).scrollTop();
+  //animation on Scroll
+  $scope.animateHeader = function() {
+    var doc = document,
+      elem = doc.documentElement,
+      header = doc.querySelector('.navbar-inverse'),
+      didScroll = false,
+      changeEffect = 220;
 
-    scrollLink.each(function() {
-
-      var sectionOffset = $(this.hash).offset().top - 20;
-
-      if ( sectionOffset <= scrollbarLocation ) {
-        $(this).parent().addClass('active');
-        $(this).parent().siblings().removeClass('active');
+    window.addEventListener('scroll', function(e) {
+      if (!didScroll) {
+        didScroll = true;
+        setTimeout($scope.scrollPage, 250);
       }
+    }, false);
 
-      // navbar fade-in/fade-out
-      if (scrollbarLocation > 550) {
-        $('.navbar').fadeIn();
+    $scope.scrollPage = function() {
+      var sy = window.scrollY;
+      if (sy >= changeEffect) {
+        $('.navbar-inverse').addClass('navbar-animated');
       } else {
-        $('.navbar').fadeOut();
+        $('.navbar-inverse').removeClass('navbar-animated');
       }
-    });
+      didScroll = false;
+    };
+  };
 
+  $scope.animateHeader();
+})
+
+//home controller
+.controller('homeCtrl', function($scope) {
+  $scope.title = "Welcome!";
+  $scope.description = "Sit back, relax and explore some amazing portfolios!";
+})
+
+//about controller
+.controller('aboutCtrl', function($scope) {
+  $scope.aboutText = {
+    title: "Hello there!",
+    copyText: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    facebookUrl: "",
+    githubUrl: "",
+    codepenUrl: ""
+  };
+
+})
+
+//portfolio controller
+.controller('portfolioCtrl', function($scope) {
+    $scope.works = [{
+      id: 1,
+      thumbnail: 'http://placehold.it/360x400',
+      url: '',
+      caption: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+      title: 'Project 1'
+    }, {
+      id: 2,
+      thumbnail: 'http://placehold.it/360x400',
+      url: '',
+      caption: 'Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+      title: 'Project 2'
+    }, {
+      id: 3,
+      thumbnail: 'http://placehold.it/360x400',
+      url: '',
+      caption: 'Nullam id dolor id nibh ultricies vehicula ut id elit.',
+      title: 'Project 3'
+    }, {
+      id: 4,
+      thumbnail: 'http://placehold.it/360x400',
+      url: '',
+      caption: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam id dolor id nibh ultricies vehicula ut id elit.',
+      title: 'Project 4'
+    }, {
+      id: 5,
+      thumbnail: 'http://placehold.it/360x400',
+      url: '',
+      caption: 'Nullam id dolor id nibh ultricies vehicula ut id elit.',
+      title: 'Project 5'
+    }, {
+      id: 6,
+      thumbnail: 'http://placehold.it/360x400',
+      url: '',
+      caption: 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus.',
+      title: 'Project 6'
+    }];
+  })
+  //contact controller
+  .controller('contactCtrl', function($scope) {
+    $scope.contact = {};
   });
-  // Navbar popup
-  $(window).on('sections', function() {
-
-  });
-
-});
-// end of smooth scrolling
-
-
-// word substitution
-var length = $(".scroll-words").length;
-var p = 0;
-var u = 0;
-while(u<10000) {
-for(i=0;i<length;i++){
-  $( ".scroll-list" ).animate({
-    marginTop: "-"+(i*2)+".55em"
-  }, 2000 );
-  p = i+1;
-}
-
-if(p==length){
-for(j=p-1;j>=0;j--){
-  $( ".scroll-list" ).animate({
-    marginTop: "-"+(j*2)+".55em"
-  }, 2000 );
-}
-}
-u++;
-}
